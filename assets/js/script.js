c990bfbc5bc0b656d2d170b452dcfc4a788bdf83
pinklightsaber <ctorres@idemar.cl>
2018-05-30 21:21:39 -0400

    abrir y cerrar tarjetas, y toggle class con los like corazones

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+ $(function() {
+
+ 	$('.card').on('click', function(){
+ 		$(this).toggleClass("card--open");
+ 	});
+ 	
+ 	$('.card__like').on('click', function(event){
+ 		$(this).toggleClass("card__like--red");
+ 		event.stopPropagation();
+ 		event.preventDefault();
+ 	});
+
+ 	$('.card__follow-btn').on('click', function(){
+ 		$(this).toggleClass("card__follow-btn--following");
+ 	})
+      
+  });
